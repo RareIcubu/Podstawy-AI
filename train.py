8c6a3b08bf3fb08211da70018a33ceddd71a689f
devoid-of-thought <mikynowy@gmail.com>
2025-11-27 17:26:38 +0100

    changes

--- train.py
+++ train.py
@@ -4,12 +4,13 @@ import funkcje as fa
 import numpy as np
 import minst as mn
 import mlp
+import time
 
 def create_model(input_size,output_size=10):
-    model = sc.MLP(learning_rate=0.1)
-    model.add_layer(wr.Warstwa(input_size=input_size, layer_size=128, activation=fa.relu, activation_prime=fa.relu_derivative, weights_init='he'))
-    model.add_layer(wr.Warstwa(input_size=128, layer_size=64, activation=fa.relu, activation_prime=fa.relu_derivative, weights_init='he'))
-    model.add_layer(wr.Warstwa(input_size=64, layer_size=output_size, activation=fa.softmax, activation_prime=fa.derivative_one, weights_init='he'))
+    model = sc.MLP(learning_rate=0.2)
+    model.add_layer(wr.Warstwa(input_size=input_size, layer_size=32, activation=fa.relu, activation_prime=fa.relu_derivative, weights_init='he'))
+    model.add_layer(wr.Warstwa(input_size=32, layer_size=16, activation=fa.relu, activation_prime=fa.relu_derivative, weights_init='he'))
+    model.add_layer(wr.Warstwa(input_size=16, layer_size=output_size, activation=fa.softmax, activation_prime=fa.derivative_one, weights_init='he'))
     return model
 def rozdzielenie_danych(dane):
     dane_X = []
@@ -33,13 +34,22 @@ model = create_model(784)
 save_path = 'model_mnist.npz'
 dane_wejsciowe, labels = rozdzielenie_danych(dataset_treningowy)
 
-model_stary = mlp.MLP([784, 128, 64, 10], learning_rate=0.1)
-model_stary.fit(dane_wejsciowe, labels, epochs=1000, print_every=500)
-model.fit(dane_wejsciowe, labels, epochs=1000, print_every=500)
+
+model_stary = mlp.MLP([784, 32, 16, 10], learning_rate=0.2)
+start_time_1 = time.time()
+model_stary.fit(dane_wejsciowe, labels, epochs=500, print_every=10)
+end_time_1 = time.time()
+start_time_2 = time.time()
+model.fit(dane_wejsciowe, labels, epochs=500, print_every=10)
+end_time_2 = time.time()
+
+print(f"Czas trenowania starego modelu: {end_time_1 - start_time_1} ms")
+print(f"Czas trenowania nowego modelu: {end_time_2 - start_time_2} ms")
 model.save_model(save_path)
+
 for i, test_set in enumerate([dataset_treningowy,dataset_testowy_1, dataset_testowy_2, dataset_testowy_3,dataset_testowy_wszystkie,dataset_testowy_pozostałe], start=1):
     test_X, test_y = rozdzielenie_danych(test_set)
     accuracy = model.evaluate(test_X, test_y)
     accuracy_stary = model_stary.evaluate(test_X, test_y)
-    print(f"Dokładność (stary model) na zestawie testowym {i}: {accuracy_stary * 100}%")
-    print(f"Dokładność na zestawie testowym {i}: {accuracy * 100}%")
+    print(f"Dokładność (stary model) na zestawie testowym {i} : {accuracy_stary * 100}%")
+    print(f"Dokładność na zestawie testowym {i} : {accuracy * 100}%")
